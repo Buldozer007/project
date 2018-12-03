d048ada9b74312ee2baac094ed9143b9b40c003a
Your Name <you@example.com>
2018-12-03 23:48:56 +0400

    Finnaly class.hunter.js is working

--- classes/class.hunter.js
+++ classes/class.hunter.js
@@ -1,9 +1,9 @@
 class Hunter extends Parent {
     constructor(x, y, index) {
         super(x, y, index);
-        this.stamina = 10;
-        this.bullets = 12;
-        this.food = 15;
+        this.stamina = 6;
+        this.bullets = 8;
+        this.food = 9;
         matrix[this.x][this.y] = this.index;
     }
     chooseCell(ch) {
@@ -14,37 +14,35 @@ class Hunter extends Parent {
         super.getNewCoorinates();
     }
     move() {
-        var cell = random(this.chooseCell(0));
-        if (this.stamina > 0) {
+        var cell = random(this.chooseCell(0 || 1));
+        if (cell && this.stamina > 0 ) {
             this.stamina--;
             matrix[this.y][this.x] = 0;
             this.x = cell[0]; this.y = cell[1];
             matrix[this.y][this.x] = 4;
-        }
+            }
     }
     eat() {
         if (this.stamina < 2) {
-            this.food -= 2;
+            this.food -= 1.5;
             this.stamina++;
         }
     }
     kill() {
         var cell = random(this.chooseCell(2 || 3));
-        if (this.food < 3) {
+        if (this.food < 2 && cell > 0) {
             this.bullets--;
-            this.x = cell[0]; this.y = cell[1];
-            matrix[this.y][this.x] = 3;
-            if (matrix[this.x][this.y] == 2) {
+            if (cell[0] && cell[1] == 2) {
                 for (var i in grass_eaterArr) {
-                    if (grass_eaterArr[i].x == this.x && grass_eaterArr[i].y == this.y) {
+                    if (grass_eaterArr[i].x == cell[0] && grass_eaterArr[i].y == cell[1]) {
                         grass_eaterArr.splice(i, 1);
                     }
                     this.food++;
                 }
             }
-            else if (matrix[this.x][this.y] == 3) {
+            else if (cell[0] && cell[1] == 3) {
                 for (var i in predatorArr) {
-                    if (predatorArr[i].x == this.x && predatorArr[i].y == this.y) {
+                    if (predatorArr[i].x == cell[0] && predatorArr[i].y == cell[1]) {
                         predatorArr.splice(i, 1);
                     }
                 }
